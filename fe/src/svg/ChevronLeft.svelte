c787cb18da53b1e10012a7ae0818bd6fc44b7178
Eduards Egle <eduards@egle.me>
2021-03-11 20:54:49 +0200

    Datepicker style upgrades

--- fe/src/svg/ChevronLeft.svelte
+++ fe/src/svg/ChevronLeft.svelte
@@ -1,9 +1,15 @@
 <script>
   export let width = 24;
   export let height = 24;
+  export let color = 'black';
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 24 24"
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill={color}
+  {width}
+  {height}
+  viewBox="0 0 24 24"
   ><path
     d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
   /></svg
--- fe/src/svg/ChevronRight.svelte
+++ fe/src/svg/ChevronRight.svelte
@@ -1,8 +1,14 @@
 <script>
   export let width = 24;
   export let height = 24;
+  export let color = 'black';
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 24 24"
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill={color}
+  {width}
+  {height}
+  viewBox="0 0 24 24"
   ><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" /></svg
 >
